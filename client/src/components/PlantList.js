b21ca4c9ac446c987dbb7ad21579a207f9c3c351
EmilyLow <emlow@me.com>
2020-08-14 19:53:59 -0500

    Filter that works once

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -6,7 +6,10 @@ export default class PlantList extends Component {
   constructor() {
     super();
     this.state = {
-      plants: []
+      plants: [],
+      rawPlants: [],
+      filterField: "",
+      activeFilter: ""
     }
   }
 
@@ -19,19 +22,62 @@ export default class PlantList extends Component {
       .get("http://localhost:3333/plants")
       .then((res) => {
           
-          this.setState({plants: res.data.plantsData});
+          this.setState({plants: res.data.plantsData, rawPlants: res.data.plantsData});
           console.log(this.state.plants);
+          
         }
       ) 
       .catch((err) => console.log("Plant data error"));
       
     }
 
+    filterPlants() {
+      // console.log("Updated");
+      if(this.state.activeFilter != "") {
+        this.setState({plants: this.state.rawPlants});
+        console.log("filtering");
+        let filteredPlants = this.state.plants.filter((plant) => {
+
+          return plant.name.includes(this.state.activeFilter);
+        }
+        );
+        this.setState({plants: filteredPlants})
+      };
+      
+    }
+
+    //Attempting filter
+   handleSubmit = e => {
+     e.preventDefault();
+     console.log("submit");
+     this.setState({activeFilter: this.state.filterField});
+    //  console.log("Active filter", this.state.activeFilter);
+    this.filterPlants();
+   }
+   handleChange = e => {
+    console.log("change");
+    this.setState({filterField: e.target.value});
+    // console.log("Filter field,", this.state.filterField);
+  }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+  //Altering for Filter Stretch
   render() {
     return (
       <main className="plant-list">
+        {/* Filter stretch begins */}
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Filter:
+            <input
+              name = "filterBy"
+              value = {this.state.filterField}
+              onChange = {this.handleChange}
+             />
+          </label>
+          <button>Submit</button>
+        </form>
+        {/* Filter stretch ends */}
         {this.state?.plants?.map((plant) => (
           <div className="plant-card" key={plant.id}>
             <img className="plant-image" src={plant.img} alt={plant.name} />
